2c2c35954c25bb283a913c84851f893862f1ded6
Sushane Yadav <78971334+shaneydv@users.noreply.github.com>
2023-12-27 14:44:06 +0530

    initial commit

---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl: "https://cloud.appwrite.io/v1",
+    appwriteProjectId: "658814e0ae47936d5b9e",
+    appwriteDatabaseId: "658815d61e6ca1641c08",
+    appwriteCollectionId: "658815f4af562e7c4310",
+    appwriteBucketId: "65881717e416b0bfab4f",
+}
+
+export default conf
